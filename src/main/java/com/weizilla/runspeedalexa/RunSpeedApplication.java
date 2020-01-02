1016aca58ad670f8588c41adb0bc042c304203ce
Wei Yang <wei@weizilla.com>
2020-01-01 20:19:58 -0800

    Initial commit, start doing some stuff

---
+++ src/main/java/com/weizilla/runspeedalexa/RunSpeedApplication.java
@@ -0,0 +1,11 @@
+package com.weizilla.runspeedalexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunSpeedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunSpeedApplication.class, args);
+    }
+}
